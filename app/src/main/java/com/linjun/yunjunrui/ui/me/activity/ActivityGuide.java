dd679b5cadc7134ec457bf55ea7e16866e1f74ad
linjun <10454735402@qq.com>
2017-08-02 17:57:49 +0800

    优化代码

--- app/src/main/java/com/linjun/yunjunrui/ui/me/activity/ActivityGuide.java
+++ app/src/main/java/com/linjun/yunjunrui/ui/me/activity/ActivityGuide.java
@@ -1,13 +1,29 @@
 package com.linjun.yunjunrui.ui.me.activity;
 
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.linjun.yunjunrui.R;
 import com.linjun.yunjunrui.ui.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 作者：林俊 on 2017/7/31
  * 作用：
  */
-public class ActivityGuide extends BaseActivity{
+public class ActivityGuide extends BaseActivity {
+    @BindView(R.id.iv_guideback)
+    ImageView ivGuideback;
+    @BindView(R.id.tv_device_name)
+    TextView tvDeviceName;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_guide;
@@ -22,9 +38,15 @@ public class ActivityGuide extends BaseActivity{
     protected void bindEvent() {
 
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    @OnClick(R.id.iv_guideback)
+    public void onViewClicked() {
+        this.finish();
     }
 }
